faca3c593237594daeb78b4439ebf0c087ad46bc
Jake <56329025+jakeo-dev@users.noreply.github.com>
2024-11-25 06:09:08 -0800

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        B52s: ['B52s', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+}
+
